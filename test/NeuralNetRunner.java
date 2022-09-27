9a017dc29ab1d84f519ae6dd8a2ecf6dee07a22f
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2022-09-26 21:31:26 -0700

    Update NeuralNetRunner.java

--- test/NeuralNetRunner.java
+++ test/NeuralNetRunner.java
@@ -10,7 +10,15 @@ public class NeuralNetRunner {
     double[] targetSet;
     double totalError;
 
-    NeuralNet nn = new NeuralNet(); // need to pass parameters
+    NeuralNet nn = new NeuralNet(
+            2,
+            4,
+            0.2,
+            0,
+            0,
+            1,
+            false
+    ); // need to pass parameters
 
     public NeuralNetRunner(boolean isBipolar) {
         totalError = 0;
@@ -21,25 +29,33 @@ public class NeuralNetRunner {
         trainingSet = new double[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         targetSet = new double[]{-1, 1, 1, -1};
         }
+
+        nn.initializeWeights();
     }
 
     private void printInfo(){
 
     }
 
+    private void train(){
+
+    }
+
     public void run(){
+        totalError = THRESHOLD + 1;
         // outer loop: to check whether meet the break condition
-        while(...){
+        int epoch = 0;
+        while(totalError > THRESHOLD && epoch < 5000){
             // (1) reach the number of epochs we want (2) total error is acceptable
             totalError = 0;
             // inner loop: train the training set once
             for (int i = 0; i < targetSet.length; ++i) {
-                totalError += nn.train(trainingSet[i], targetSet[i]);
-                printInfo();
+                double error = nn.train(trainingSet[i], targetSet[i]);
+                totalError += error;
             }
+            System.out.println("The total error of " + ++epoch +" epoch: " + totalError);
         }
     }
-
 
     public static void main(String[] args) {
         NeuralNetRunner runner = new NeuralNetRunner(false);
