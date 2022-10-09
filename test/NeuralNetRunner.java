b00785779bd6ccfe51249b0649f7af69c880ba9f
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2022-10-08 20:05:41 -0700

    Update NeuralNetRunner.java

--- test/NeuralNetRunner.java
+++ test/NeuralNetRunner.java
@@ -10,26 +10,35 @@ public class NeuralNetRunner {
     double[] targetSet;
     double totalError;
 
-    NeuralNet nn = new NeuralNet(
-            2,
-            4,
-            0.2,
-            0,
-            0,
-            1,
-            false
-    ); // need to pass parameters
+    NeuralNet nn;
 
     public NeuralNetRunner(boolean isBipolar) {
         totalError = 0;
         if(!isBipolar){
-          trainingSet = new double[][]{{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-          targetSet = new double[]{0, 1, 1, 0};
+            trainingSet = new double[][]{{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+            targetSet = new double[]{0, 1, 1, 0};
+            nn = new NeuralNet(
+                    2,
+                    4,
+                    0.2,
+                    0,
+                    0,
+                    1,
+                    false
+            );
         } else{
-        trainingSet = new double[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-        targetSet = new double[]{-1, 1, 1, -1};
+            trainingSet = new double[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+            targetSet = new double[]{-1, 1, 1, -1};
+            nn = new NeuralNet(
+                    2,
+                    4,
+                    0.2,
+                    0,
+                    -1,
+                    1,
+                    true
+            );
         }
-
         nn.initializeWeights();
     }
 
@@ -45,20 +54,21 @@ public class NeuralNetRunner {
         totalError = THRESHOLD + 1;
         // outer loop: to check whether meet the break condition
         int epoch = 0;
-        while(totalError > THRESHOLD && epoch < 5000){
+        while(totalError > THRESHOLD){
             // (1) reach the number of epochs we want (2) total error is acceptable
             totalError = 0;
             // inner loop: train the training set once
             for (int i = 0; i < targetSet.length; ++i) {
                 double error = nn.train(trainingSet[i], targetSet[i]);
                 totalError += error;
+//                System.out.println(error);
             }
-            System.out.println("The total error of " + ++epoch +" epoch: " + totalError);
+            System.out.println("The total error of " + totalError +" epoch: " + ++epoch);
         }
     }
 
     public static void main(String[] args) {
-        NeuralNetRunner runner = new NeuralNetRunner(false);
+        NeuralNetRunner runner = new NeuralNetRunner(true);
         runner.run();
     }
 }
